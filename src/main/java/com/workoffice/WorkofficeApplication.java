c712fe9493ad0e047f1be47e6d6dfc9d15139b90
pawfa <fajfer.pawel@gmail.com>
2017-11-05 20:54:41 +0100

    Created project

---
+++ src/main/java/com/workoffice/WorkofficeApplication.java
@@ -0,0 +1,12 @@
+package com.workoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkofficeApplication.class, args);
+	}
+}
